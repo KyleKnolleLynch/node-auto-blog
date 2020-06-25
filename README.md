6110154fc824dc4fcaf1cf2e0cc7c893fd41be09
Kyle Lynch <kylelynch104@gmail.com>
2020-06-25 16:43:36 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#  node auto blog
+
+## automotive and racing blog
+### built with 
+*  node js
+*  express
+*  mongoose
+*  ejs
---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+const { render } = require('ejs');
+require('dotenv').config();
+
+//  express app
+const app = express();
+
+//  connect to mongodb
+const dbURI = process.env.MONGO_KEY;
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+//  register view engine
+app.set('view engine', 'ejs');
+
+//  middleware and static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+//  routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+  // res.send('<p>About Page</p>');
+  res.render('about', { title: 'About' });
+});
+
+//  redirects
+// app.get('/about-us', (req, res) => {
+//   res.redirect('/about');
+// });
+
+//  blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: '', blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new Blog' });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//  404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
